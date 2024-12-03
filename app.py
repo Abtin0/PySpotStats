c4a11a98576ad3210f86e55f40088f98f7dd43c2
Abtin <a.fouladizadeh@gmail.com>
2024-12-03 18:47:00 -0500

    🔧 Updated Login Cache Handling

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from flask import Flask, session, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
-from spotipy.cache_handler import FlaskSessionCacheHandler
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
 
+# Environment variables for Spotify credentials
 client_id = os.environ.get("PYSPOTSTATS_CLIENT_ID")
 client_secret = os.environ.get("PYSPOTSTATS_CLIENT_SECRET")
 redirect_uri = "http://localhost:5000/callback"
 scope = "playlist-read-private,streaming"
 
-cache_handler = FlaskSessionCacheHandler(session)
+# Directory to store the cache
+CACHE_DIR = ".spotify_cache"
+if not os.path.exists(CACHE_DIR):
+    os.makedirs(CACHE_DIR)
+
+# File-based caching for SpotifyOAuth
+cache_path = os.path.join(CACHE_DIR, "token_cache")
 oauth = SpotifyOAuth(
     client_id=client_id,
     client_secret=client_secret,
     redirect_uri=redirect_uri,
     scope=scope,
-    cache_handler=cache_handler,
+    cache_path=cache_path,
     show_dialog=True,
 )
 
@@ -27,7 +33,7 @@ sp = Spotify(auth_manager=oauth)
 
 @app.route("/")
 def index():
-    if not oauth.validate_token(cache_handler.get_cached_token()):
+    if not oauth.validate_token(oauth.get_cached_token()):
         auth_url = oauth.get_authorize_url()
         return render_template("index.html", auth_url=auth_url)
     return redirect(url_for("get_playlists"))
@@ -40,12 +46,14 @@ def callback():
 
 
 @app.route("/get_playlists")
-@app.route("/get_playlists")
 def get_playlists():
+    if not oauth.validate_token(oauth.get_cached_token()):
+        auth_url = oauth.get_authorize_url()
+        return render_template("index.html", auth_url=auth_url)
     playlists = sp.current_user_playlists()
     playlists_info = []
     for pl in playlists['items']:
-        if pl and 'name' in pl and pl['name']:  # Check if 'pl' is not None and has a 'name' field
+        if pl and 'name' in pl and pl['name']:
             playlists_info.append((pl['name'], pl['external_urls']['spotify']))
     playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
 
@@ -54,10 +62,11 @@ def get_playlists():
 
 @app.route("/logout")
 def logout():
-    session.clear()
+    # Clear the cache by deleting the file
+    if os.path.exists(cache_path):
+        os.remove(cache_path)
     return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
